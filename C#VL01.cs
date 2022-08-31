0690ba8f10cbef13901a37511921a07aede9efd8
Khonphachoth <111957636+Khonphachoth@users.noreply.github.com>
2022-08-31 14:47:22 +0700

    Update C#VL01.cs

--- C#VL01.cs
+++ C#VL01.cs
@@ -1,19 +1,13 @@
 using System;
-namespace VL01
+public class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void main(string[] args)
-        {
-            var s = Console.ReadLine();
-            var s1 = s.Split(' ');
-            int a = Convert.ToInt32(s1[0]);
-            int b = Convert.ToInt32(s1[1]);
-            for (int i = a; i <= b; i++)
-            {
-                Console.Write("{0}", i);
-            }
-            Console.WriteLine();
-        }
+       string s1 = Console.ReadLine();
+            var s2 = s1.Split(' ');
+            int a = Convert.ToInt32(s2[0]);
+            int b = Convert.ToInt32(s2[1]);
+            for (int i = a; i <= b; i++)    
+            Console.Write("{0} ", i);
     }
 }
