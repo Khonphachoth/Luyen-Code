2c46fc6165daca78cb07106f7a260b2d0c645a97
Khonphachoth <111957636+Khonphachoth@users.noreply.github.com>
2022-09-09 14:14:01 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DK08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var s1 = s.Split(' ');
+            int a = Convert.ToInt32(s1[0]);
+            int b = Convert.ToInt32(s1[1]);
+            int max;
+            if (a > b) max = a;
+            else max = b;
+            Console.WriteLine(max);
+        }
+    }
+}
