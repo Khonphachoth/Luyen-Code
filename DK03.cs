36d66800fbd4de390177ae7fae206d6183739f30
Khonphachoth <111957636+Khonphachoth@users.noreply.github.com>
2022-09-09 14:19:58 +0700

    Add files via upload

---
+++ DK03.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace DK03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            var s1 = s.Split(' ');
+            int a = Convert.ToInt32(s1[0]);
+            int b = Convert.ToInt32(s1[1]);
+            Console.WriteLine(Math.Abs(a - b));
+        }
+    }
+}
---
+++ VT02.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace baiVT02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            int n = Convert.ToInt32(s);
+            var s1 = Console.ReadLine();
+            var s2 = s1.Split(' ');
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = Convert.ToInt32(s2[i]);
+            int max = a[0];
+            for (int i = 0; i < n; i++)
+                if (max < a[i]) max = a[i];
+            ///Console.WriteLine(max);
+            int j;
+            Array.Sort(a);
+            for(j=n-1; j>=0;j--)
+                if (a[j]<max)
+                {
+                    Console.WriteLine(a[j]);
+                    break;
+                }
+            if (a[j] == max) Console.WriteLine("NOT FOUND");
+        }
+    }
+}
